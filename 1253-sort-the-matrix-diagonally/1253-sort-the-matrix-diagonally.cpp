523ccda4f507d5508fbc1a7b03843a70266566d0
Hamza Arif <hamza81611@gmail.com>
2025-08-28 11:19:59 +0530

    Time: 3 ms (59.68%), Space: 13.7 MB (65.5%) - LeetHub

---
+++ 1253-sort-the-matrix-diagonally/1253-sort-the-matrix-diagonally.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
+        unordered_map<int,priority_queue<int, vector<int> , greater<int>>> m;
+        for(int i=0;i<mat.size();i++){
+            for(int j=0;j<mat[0].size();j++){
+                int d= i-j;
+                m[d].push(mat[i][j]);
+            }
+        }
+        for(int i=0;i<mat.size();i++){
+            for(int j=0;j<mat[0].size();j++){
+                mat[i][j]=m[i-j].top();
+                m[i-j].pop();
+            }
+        }
+        return mat;
+    }
+};
